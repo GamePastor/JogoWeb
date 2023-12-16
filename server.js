0fb296e1ea496be58a27b43640639396bad55f41
Lucas Moreira <131417982+LucasVPastor@users.noreply.github.com>
2023-12-16 03:28:06 -0300

    sf

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 // Servir arquivos estáticos
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Conectar-se ao WebSocket
+// Configurar uma rota para a rota raiz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Array para armazenar as posições dos jogadores
+const playerPositions = {
+    player1: { x: 0, y: 0 },
+    player2: { x: 4, y: 4 },
+};
+
+// Lógica para manipular conexões de clientes
 io.on('connection', (socket) => {
     console.log('Um jogador se conectou');
 
-    // Escutar atualizações de posição
+    // Enviar a posição inicial do jogador para o cliente recém-conectado
+    socket.emit('initialPosition', playerPositions);
+
+    // Escutar atualizações de posição do cliente
     socket.on('updatePosition', (data) => {
-        // Enviar a atualização para os outros jogadores
+        // Atualizar a posição do jogador
+        playerPositions[data.player] = data.position;
+
+        // Transmitir a atualização de posição para os outros jogadores
         socket.broadcast.emit('updatePosition', data);
     });
 
-    // Desconectar jogador
+    // Lidar com a desconexão do jogador
     socket.on('disconnect', () => {
         console.log('Um jogador se desconectou');
     });
